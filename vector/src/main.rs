9c782066d7c60e875320b917b5a12b9f1e7454dc
Sthitaprajna Sahu <sthitasahu074@gmail.com>
2024-11-09 00:05:23 +0530

    solved a basiic practice problem

--- vector/src/main.rs
+++ vector/src/main.rs
@@ -2,7 +2,37 @@ fn main() {
     let mut vec=Vec::new();
     vec.push(1);
     vec.push(2);
-    vec.push(7);
+    vec.push(3);
+    vec.push(4);
+    vec.push(5);
 
-    println!("{:?}",vec);
+    println!("{:?}",&vec);
+
+    let  mut new_vec =even_values(vec);
+    even_values_by_reference(&mut new_vec);
+    println!("New vector is {:?}",new_vec);
+}
+
+fn even_values(v:Vec<i32>)->Vec<i32>{
+    let mut  new_vec=Vec::new();
+    for value in v{
+        if value%2==0{
+            new_vec.push(value);
+        }
+    }
+    new_vec
+}
+
+fn even_values_by_reference(v:&mut Vec<i32> ){
+
+    let mut i=0;
+    while i<v.len(){
+        if v[i]%2!=0{
+            v.remove(i);
+        }
+        else{
+            i+=1;
+        }
+    }
+
 }
