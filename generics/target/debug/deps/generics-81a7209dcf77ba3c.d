538c087467a883f01a31116fb704fc7288597547
Sthitaprajna Sahu <sthitasahu074@gmail.com>
2024-11-11 23:53:56 +0530

    added generics

---
+++ generics/target/debug/deps/generics-81a7209dcf77ba3c.d
@@ -0,0 +1,5 @@
+/mnt/c/Users/KIIT0001/Downloads/rust/generics/target/debug/deps/generics-81a7209dcf77ba3c: src/main.rs
+
+/mnt/c/Users/KIIT0001/Downloads/rust/generics/target/debug/deps/generics-81a7209dcf77ba3c.d: src/main.rs
+
+src/main.rs:
---
+++ generics/target/debug/generics.d
@@ -0,0 +1,1 @@
+/mnt/c/Users/KIIT0001/Downloads/rust/generics/target/debug/generics: /mnt/c/Users/KIIT0001/Downloads/rust/generics/src/main.rs
