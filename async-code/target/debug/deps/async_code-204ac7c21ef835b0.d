409e646489d71502346d6d4c537ea061c91e258d
Sthitaprajna Sahu <sthitasahu074@gmail.com>
2024-12-10 00:34:14 +0530

    handled a get and post request asynchronously in rust

---
+++ async-code/target/debug/deps/async_code-204ac7c21ef835b0.d
@@ -0,0 +1,5 @@
+C:\Users\KIIT0001\Downloads\rust\async-code\target\debug\deps\async_code-204ac7c21ef835b0.exe: src/main.rs
+
+C:\Users\KIIT0001\Downloads\rust\async-code\target\debug\deps\async_code-204ac7c21ef835b0.d: src/main.rs
+
+src/main.rs:
