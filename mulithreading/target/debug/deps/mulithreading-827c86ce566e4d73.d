42f2e2ade594a710e4ca9303548996fa716445d1
Sthitaprajna Sahu <sthitasahu074@gmail.com>
2024-12-09 23:56:41 +0530

    :ad files for multithreading

---
+++ mulithreading/target/debug/deps/mulithreading-827c86ce566e4d73.d
@@ -0,0 +1,5 @@
+C:\Users\KIIT0001\Downloads\rust\mulithreading\target\debug\deps\mulithreading-827c86ce566e4d73.exe: src/main.rs
+
+C:\Users\KIIT0001\Downloads\rust\mulithreading\target\debug\deps\mulithreading-827c86ce566e4d73.d: src/main.rs
+
+src/main.rs:
---
+++ mulithreading/target/debug/mulithreading.d
@@ -0,0 +1,1 @@
+C:\Users\KIIT0001\Downloads\rust\mulithreading\target\debug\mulithreading.exe: C:\Users\KIIT0001\Downloads\rust\mulithreading\src\main.rs
