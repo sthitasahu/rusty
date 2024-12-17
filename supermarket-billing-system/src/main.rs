b99453e1537ea4d36cca87f61298b27ace9bbb9b
Sthitaprajna Sahu <sthitasahu074@gmail.com>
2024-12-18 00:29:41 +0530

    completed supermarket billing application

--- supermarket-billing-system/src/main.rs
+++ supermarket-billing-system/src/main.rs
@@ -1,38 +1,43 @@
 mod order;
 mod product;
+mod utils;
+
 use order::place_order;
 use product::show_products;
+
 use inquire::Select;
 
 fn main() {
-    println!("---------------------------------------------------");
-    println!("Welcome to the supermarket billing system in rust");
-    println!("--------------------------------------------------\n");
-    println!("Menu \n1 .Press A to place an order \n2. Press P to show products list\n3. Press E to exit \n");
+    println!("-------------------------------------------");
+    println!("Welcome to Schrute Farms\nBy Dwight Schrute");
+    println!("-------------------------------------------\n");
     menu();
 }
 
 fn menu(){
-    let options=vec!["Place order","Select Product list","Exit"];
-    let input=Select::new("Menu",options.clone()).prompt();
+    let options = vec![
+        "Place order",
+        "See Product List",
+        "Exit"
+    ];
 
-    match input{
-        Ok(input)=>{
-            if (input.eq(options[0])){
+    let option = Select::new("Menu:", options.clone()).prompt();
+
+    match option {
+        Ok(option) => {
+    
+            if  option.eq(options[0]) {
                 place_order();
             }
-            else if (input.eq(options[1])){
+            else if option.eq(options[1]) {
                 show_products();
             }
-            else if (input.eq(options[2])){
-                println!("Exiting the program");
+            else if option.eq(options[2]) {
                 return;
             }
         },
-        Err(err)=>{
-            println!("Err while reading choice: {}",err);
+        Err(err) => {
+            println!("Err while reading choice: {}", err);
         },
     }
-
 }
-
--- supermarket-billing-system/src/product.rs
+++ supermarket-billing-system/src/product.rs
@@ -1,3 +1,26 @@
+use::std::error::Error;
+
+use csv;
+use crate::utils::ProductInfo;
+
+
+fn read_from_file(path: &str) -> Result<(), Box<dyn Error>> {
+    let mut reader = csv::Reader::from_path(path)?;
+
+    let headers = reader.headers()?;
+    println!("{} {}", headers.get(0).unwrap_or("-"), headers.get(1).unwrap_or("-"));
+
+    for result in reader.deserialize() {
+        let record: ProductInfo = result?;
+        println!("{}", record);
+    }
+
+    Ok(()) 
+}
+
+
 pub fn show_products(){
-    println!("You are seeing the products");
+    if let Err(e) = read_from_file("./data/products.csv") {
+        eprintln!("{}", e);
+    }
 }
